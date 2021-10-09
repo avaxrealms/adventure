6b26c9ab30197a925334134cbb33170b58b15186
plinki <geolazaria@gmail.com>
2021-10-09 21:47:10 +0300

    separate tests, time-dependent tests now work properly

--- test/adventure-test.js
+++ test/adventure-test.js
@@ -2,59 +2,86 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Adventure", function () {
-  let _adv;
-  let adv;
-  let level;
+	let _adv;
+	let adv;
+	let level;
 
-  before(async function () {
-    _adv = await ethers.getContractFactory("Adventure");
-    adv = await _adv.deploy();
+    async function mineBlocks(seconds) {
+        await network.provider.send("evm_increaseTime", [seconds]);
+        await network.provider.send("evm_mine")
+    }
 
-    _rg = await ethers.getContractFactory("RealmGold");
-    rg = await _rg.deploy(adv.address);
+	before(async function () {
+		_adv = await ethers.getContractFactory("Adventure");
+		adv = await _adv.deploy();
 
-    _attr = await ethers.getContractFactory("adventure_attributes");
-    attr = await _attr.deploy(adv.address);
+		_rg = await ethers.getContractFactory("RealmGold");
+		rg = await _rg.deploy(adv.address);
 
-    _craft_m = await ethers.getContractFactory("adventure_crafting_materials");
-    craft_m = await _craft_m.deploy(adv.address, attr.address);
+		_attr = await ethers.getContractFactory("adventure_attributes");
+		attr = await _attr.deploy(adv.address);
 
-    _sd = await ethers.getContractFactory("adventure_dungeon_snowbridge");
-    sd = await _sd.deploy(adv.address, attr.address, craft_m.address);
+		_craft_m = await ethers.getContractFactory(
+			"adventure_crafting_materials"
+		);
+		craft_m = await _craft_m.deploy(adv.address, attr.address);
 
-    await rg.deployed().then(async () => {
-      await adv.setGoldContract(rg.address);
+		_sd = await ethers.getContractFactory("adventure_dungeon_snowbridge");
+		sd = await _sd.deploy(adv.address, attr.address, craft_m.address);
+
+		await rg.deployed().then(async () => {
+			await adv.setGoldContract(rg.address);
+		});
+
+		await sd.deployed().then(async () => {
+			await craft_m.grantRole(
+				ethers.utils.keccak256(
+					ethers.utils.toUtf8Bytes("MINTER_CONTRACT")
+				),
+				sd.address
+			);
+			await adv.grantRole(
+				ethers.utils.keccak256(
+					ethers.utils.toUtf8Bytes("MANAGING_CONTRACT")
+				),
+				sd.address
+			);
+		});
+	});
+
+	it("Should summon an adventurer", async function () {
+		await adv.summon(8).then(async () => {
+			expect((await adv.summoner(0))[3]).to.equal(0x1);
+		});
+	});
+
+    it("Should send the summoner on an adventure", async function () {
+        let daysToPass = 60;
+
+        for (let i = 0; i < daysToPass; i++) {
+            await adv.adventure(0);
+            await mineBlocks(86400);
+        }
     });
 
-    await sd.deployed().then(async () => {
-      await craft_m.grantRole(
-        ethers.utils.keccak256(ethers.utils.toUtf8Bytes("MINTER_CONTRACT")),
-        sd.address
-      );
-      await adv.grantRole(
-        ethers.utils.keccak256(ethers.utils.toUtf8Bytes("MANAGING_CONTRACT")),
-        sd.address
-      );
-    });
-  });
+    it("Should level the summoner up", async function () {
+        for (let i = 0; i < 3; i++) {
+            await adv.level_up(0);
+        }
 
-  it("Should summon an adventurer", async function () {
-    await adv.summon(8).then(async () => {
-      await adv.adventure(0);
-      await adv.level_up(0);
-      await adv.level_up(0);
-      await adv.level_up(0);
-      expect((await adv.summoner(0))[3]).to.equal(0x4);
+        console.log(await adv.summoner(0));
+        expect((await adv.summoner(0))[3]).to.equal(0x4);
     });
-  });
 
-  it("Should increase attributes and adventure", async function () {
-    await attr.point_buy(0, 8, 18, 15, 8, 15, 8).then(async () => {
-      await sd.adventure(0);
+	it("Should increase attributes", async function () {
+		await attr.point_buy(0, 8, 18, 15, 8, 15, 8);
+	});
+
+    it("Should adventure through the snowbridge", async function () {
+        await sd.adventure(0);
     });
-  });
 
-  it("Should have a balance of 1 Craft (I)", async function () {
-    expect(await craft_m.balanceOf(0)).to.equal(0x1);
-  });
+	it("Should have a balance of 1 Craft (I)", async function () {
+		expect(await craft_m.balanceOf(0)).to.equal(0x1);
+	});
 });
