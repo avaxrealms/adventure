233d6ac804f4f01c9a508f1ba0c6ed47f119e9a1
chazu <chazbone@gmail.com>
2021-12-07 20:24:36 -0500

    run myth works without bullshit now - for me :troll:

--- run_myth.py
+++ run_myth.py
@@ -15,18 +15,36 @@ Run mythril
 
 import os
 
-deploying = [
-    "core/adventure.sol",
-    #"core/plunder.sol",
-    "core/RGold.sol",
-    "core/attributes.sol",
-    "dungeon/snowbridge.sol",
-    "core/attacher.sol"
-]
+contracts = []
 
-for contract in deploying:
-    output_filename = contract.split("/")[1]
+def add_contract(subpath, filename: str):
+    contracts.append({"subpath": subpath, "filename": filename})
+
+
+add_contract("core", "adventure.sol")
+add_contract(None, "plunder.sol")
+add_contract("core", "RGold.sol")
+add_contract("core", "attributes.sol")
+add_contract("dungeon", "snowbridge.sol")
+add_contract("core", "attacher.sol")
+
+
+for contract in contracts:
     print(f"Running against {contract}")
-    # os.system(f"npx hardhat flatten contracts/{contract} > flattened/{output_filename}")
+    output_path = f"flattened/{contract['filename']}"
+    if contract['subpath']:
+        os.system(f"npx hardhat flatten contracts/{contract['subpath']}/{contract['filename']} > {output_path}")
+    else:
+        os.system(f"npx hardhat flatten contracts/{contract['filename']} > {output_path}")
 
-    os.system(f"slither flattened/{output_filename} > scans/{output_filename}")
+    # TODO remove all SPDX shit from flattened
+    with open(output_path, "r") as f:
+        lines = f.readlines()
+    with open(output_path, "w") as f:
+        for line in lines:
+            if not "SPDX" in line:
+                f.write(line)
+    os.system(f"slither flattened/{contract['filename']} > scans/{contract['filename']}.slither 2>&1")
+
+
+    #os.system(f"myth a flattened/{output_filename} > scans/{output_filename}.myth")
